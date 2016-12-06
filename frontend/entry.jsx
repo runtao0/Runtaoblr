0f56beaba597ca7d32e49ecc2b0cdfd5a9b4c85b
runtao0 <ry2228@columbis.edu>
2016-12-06 11:58:30 -0500

    Add routes, view is accessible on local host

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to My Greatest Achievement</h1>, root);
+  ReactDOM.render(<h1>Welcome to My Greatest Achievement!!!</h1>, root);
 });
