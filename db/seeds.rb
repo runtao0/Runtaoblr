f5168d059f89aefe31341723ade14765553b38d6
runtao0 <ry2228@columbis.edu>
2016-12-07 14:51:40 -0500

    Seed user data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+
+User.create(username: "runtao", password: "runtao")
+User.create(username: "davidCarp", password: "davidCarp")
+User.create(username: "wes", password: "weswes")
+User.create(username: "rupaul", password: "rupaulcharles")
