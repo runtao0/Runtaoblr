cf68e36e0434114c23b2a6df22a6a56881e239c0
runtao0 <ry2228@columbis.edu>
2016-12-12 14:54:06 -0500

    Add indices to follows table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161212034603) do
+ActiveRecord::Schema.define(version: 20161212194650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,12 +23,18 @@ ActiveRecord::Schema.define(version: 20161212034603) do
     t.datetime "updated_at", null: false
   end
 
+  add_index "follows", ["sheep_id"], name: "index_follows_on_sheep_id", using: :btree
+  add_index "follows", ["sheperd_id"], name: "index_follows_on_sheperd_id", using: :btree
+
   create_table "likes", force: :cascade do |t|
     t.integer  "liked_post_id", null: false
     t.integer  "liker_id",      null: false
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
+
+  add_index "likes", ["liked_post_id"], name: "index_likes_on_liked_post_id", using: :btree
+  add_index "likes", ["liker_id"], name: "index_likes_on_liker_id", using: :btree
 
   create_table "posts", force: :cascade do |t|
     t.string   "kind",             null: false
