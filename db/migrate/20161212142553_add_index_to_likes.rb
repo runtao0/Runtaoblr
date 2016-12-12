8c06e7f44b04e5ddb0babd5f0901e3518033fa43
runtao0 <ry2228@columbis.edu>
2016-12-12 09:27:25 -0500

    Migrate likes table

---
+++ db/migrate/20161212142553_add_index_to_likes.rb
@@ -0,0 +1,6 @@
+class AddIndexToLikes < ActiveRecord::Migration
+  def change
+    add_index :likes :liker_id
+    add_index :likes :liked_post_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161209212332) do
+ActiveRecord::Schema.define(version: 20161212034603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20161209212332) do
     t.integer  "sheperd_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "liked_post_id", null: false
+    t.integer  "liker_id",      null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "posts", force: :cascade do |t|
