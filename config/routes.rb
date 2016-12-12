e122f9459128b3ac52f6c716e9578fe8d9dbc2c5
runtao0 <ry2228@columbis.edu>
2016-12-12 14:54:48 -0500

    Add routes for feed

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,23 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     resource :session, only: [:create, :destroy]
+
     resources :users, only: [:create, :show, :index] do
       member do
         post :follow
         post :unfollow
       end
+      collection do
+        get :feed
+      end
     end
-    resources :posts, only: [:create, :index, :show, :destroy, :update]
+
+    resources :posts, only: [:create, :index, :show, :destroy, :update] do
+      member do
+        post :like
+        post :unlike
+      end
+    end
   end
+
 end
