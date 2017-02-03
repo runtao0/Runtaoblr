1c343cfdc5f5bbfb4575108635a92586b09484da
Runtao0 <ry2228@columbia.edu>
2017-02-03 18:14:05 -0500

    update paperclip things

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
+  Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
 end
