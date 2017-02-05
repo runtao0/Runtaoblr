0b90105e9839864ea252c8600b75e3daa36786f3
Runtao0 <ry2228@columbia.edu>
2017-02-05 12:38:06 -0500

    Production react

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,4 +1,5 @@
 const path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
   context: __dirname,
@@ -23,4 +24,16 @@ module.exports = {
     ]
   },
   devtool: 'source-maps',
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ]
 };
