e66618330e37a52a386ab2795b4d6c9cd4ab1bfb
Runtao0 <ry2228@columbia.edu>
2017-02-05 12:53:48 -0500

    React minify again

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -7,7 +7,7 @@ module.exports = {
     path: "./app/assets/javascripts",
     filename: 'bundle.js'
   },
-  plugins:[
+  plugins: [
     new webpack.DefinePlugin({
       'process.env':{
         'NODE_ENV': JSON.stringify('production')
